df938b3b03d851b25a4a89a79e9298778b7763a5
Christophe Bontemps <christophe.bontemps@toulouse.inra.fr>
2026-03-25 14:46:12 +0600

    import OK

---
+++ code/RegressionWithNSB.R
@@ -0,0 +1,14 @@
+# Regression modelling with NSB
+# Paro
+
+# Seting up the working directory
+setwd("C:/GitMain/DataScience4OS/code")
+
+# load some packages 
+library(haven)   # if we need to load Excel , Stat, files...
+
+# Load the data
+
+Mydata <- read.csv("../Data/Household.csv")
+
+
