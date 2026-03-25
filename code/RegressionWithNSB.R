cec1df6f5977b4c0bcb8f466488c340adeb1af3f
Christophe Bontemps <christophe.bontemps@toulouse.inra.fr>
2026-03-25 15:06:36 +0600

    Changing tye=pe of variables

--- code/RegressionWithNSB.R
+++ code/RegressionWithNSB.R
@@ -6,9 +6,27 @@ setwd("C:/GitMain/DataScience4OS/code")
 
 # load some packages 
 library(haven)   # if we need to load Excel , Stat, files...
+library(tidyverse)  # Data Science package
 
-# Load the data
-
+# Step1: Load the data
 Mydata <- read.csv("../Data/Household.csv")
+
+# Data feature
+str(Mydata)
+head(Mydata)
+names(Mydata)
+
+# Step 2: Exploring the data 
+summary(Mydata)
+
+# Changing the type of Sex variable
+
+Mydata  <- Mydata %>%
+  mutate( sexF = as.factor(sex), 
+          education_level  = as.factor(education_level)
+          )
+
+
+
 
 
